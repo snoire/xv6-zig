60981a80c51fe9588603af59efb781e6627f47b6
Snoire <snoire@qq.com>
2022-06-11 22:00:07 +0800

    add fprint()

--- user/print.zig
+++ user/print.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
 const sys = @import("usys.zig");
 
-fn write(_: void, string: []const u8) error{}!usize {
-    return sys.write(1, string.ptr, string.len);
+fn write(fd: usize, string: []const u8) error{}!usize {
+    return sys.write(fd, string.ptr, string.len);
 }
 
-const Writer = std.io.Writer(void, error{}, write);
+const Writer = std.io.Writer(usize, error{}, write);
+
+pub fn fprint(fd: usize, comptime format: []const u8, args: anytype) void {
+    std.fmt.format(Writer{ .context = fd }, format, args) catch unreachable;
+}
 
 pub fn print(comptime format: []const u8, args: anytype) void {
-    std.fmt.format(Writer{ .context = {} }, format, args) catch unreachable;
+    fprint(1, format, args);
 }
--- user/sleep.zig
+++ user/sleep.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 const sys = @import("usys.zig");
-const print = @import("print.zig").print;
+const fprint = @import("print.zig").fprint;
 
 export fn main(argc: c_int, argv: [*]?[*:0]u8) noreturn {
     if (argc < 2) {
-        print("Usage: sleep <secs>\n", .{});
+        fprint(2, "Usage: sleep <secs>\n", .{});
         sys.exit(1);
     }
 
     _ = sys.sleep(std.fmt.parseInt(u8, std.mem.sliceTo(argv[1].?, 0), 0) catch |err| {
-        print("{s}\n", .{@errorName(err)});
+        fprint(2, "{s}\n", .{@errorName(err)});
         sys.exit(1);
     });
 
